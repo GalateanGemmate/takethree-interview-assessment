742b6b565579d639efaf362418264da78e9aa5f3
GGG <ggg@uoregon.edu>
2021-06-02 12:47:58 -0600

    Reorder using directives
    Move all System using directives to the very start of their file.
    
    using directives were ordered alphabetically, but the StyleCop schema
    indicates that System using directives should be before all other
    directives.

--- TechnicalChallengeApp/Controllers/CalculatorController.cs
+++ TechnicalChallengeApp/Controllers/CalculatorController.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using TechnicalChallengeApp.BusinessLogic.Calculation;
 using TechnicalChallengeApp.BusinessLogic.Session;
 using TechnicalChallengeApp.Model.Calculation;
